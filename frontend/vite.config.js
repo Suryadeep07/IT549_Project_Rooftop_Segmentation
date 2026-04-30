afaf3125ae2b67ada4d946a5363c68f68b73fb9c
Suryadeepsinh Gohil <suryadeepsinhgohil@Suryadeepsinhs-MacBook-Air.local>
2026-04-30 20:26:50 +0530

    Added frontend codefiles

---
+++ frontend/vite.config.js
@@ -0,0 +1,26 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: ['.ngrok-free.dev'],  // 👈 ADD THIS LINE
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
