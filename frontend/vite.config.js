91c754c0abfce0c22230398b84dbde34f763b751
Anushka197 <159715874+Anushka197@users.noreply.github.com>
2026-04-30 21:56:37 +0530

    Modifications for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: true, // This is fine to keep, it lets you view the app on your phone via local Wi-Fi
     port: 5173,
-    allowedHosts: ['.ngrok-free.dev'],  // 👈 ADD THIS LINE
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
   }
 })
